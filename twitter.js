53012ba62d6972c1fcd444983b74625394dc43c6
Eric Choi <eric.yongjun.choi@gmail.com>
2016-08-03 22:51:53 -0700

    complete twitter

--- twitter.js
+++ twitter.js
@@ -1,24 +1,41 @@
 var request = require('request');
+var async = require("async");
+
+var searchStr = process.argv[2];
 var creds = {
   'key': 'Zm3ipLxRCgPG4iM5CmF1YSOQ4',
   'secret': 'qvwwM81ZHDH49aTlFYgjPyBDSAn6ydr8zU5zdYOxMsLB8kqZRf',
 };
-var auth = [creds.key, creds.secret].join(':');
-var buf = Buffer.from(auth, 'ascii');
-auth = buf.toString('base64');
-console.log(auth);
 
 // auth request
-var options = {
-  headers: { 'Authorization': 'Basic ' + auth },
-  form: {grant_type:'client_credentials'}
-}
+var requestToken = function (callback) {
+  var auth = [creds.key, creds.secret].join(':');
+  var buf = Buffer.from(auth, 'ascii');
+  auth = buf.toString('base64');
+  var options = {
+    headers: { 'Authorization': 'Basic ' + auth },
+    form: {grant_type:'client_credentials'}
+  }
+  request.post('https://api.twitter.com/oauth2/token', options, callback)
+};
 
-request.post('https://api.twitter.com/oauth2/token', options,
-  function (error, response, body) {
-    //if (!error && response.statusCode == 200) {
-    console.log('err:' + error);
-    console.log('resp:' + response);
-    console.log('body:' + body);
-    //}
-  })
+var requestSearch = function (resp, body, callback) {
+  var access_token = JSON.parse(body).access_token;
+  request({
+    uri: 'https://api.twitter.com/1.1/search/tweets.json',
+    qs: { q: searchStr }
+  }, callback).auth(null, null, true, access_token);
+};
+
+var printResult = (resp, body, callback) => {
+  var statuses = JSON.parse(body).statuses;
+  statuses.forEach(function (twitter) {
+    console.log(twitter.user.name + "\t" + twitter.text)
+  });
+};
+
+async.waterfall([
+  requestToken,
+  requestSearch,
+  printResult
+]);
